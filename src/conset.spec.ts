61d154de97ebfae9f8cf995e4a0e5ab8d87d9d75
Shasak <shasak.sync@gmail.com>
2017-10-23 22:56:26 +0530

    added tests

--- src/conset.spec.ts
+++ src/conset.spec.ts
@@ -3,14 +3,33 @@ import {conset, Conset} from './conset'
 
 interface DummyItem {
     test: string,
-    data: ["dummy", "values"]
+    data: any[]
+}
+
+const dummy = <T>(key:string, data:T[]) => {
+    return {
+        test: key,
+        data: data
+    }
 }
 
 const isInstanceOfConset = <T>(obj:any): obj is Conset<T> => {
     return "map" in obj && "order" in obj && "comparator" in obj
 }
 
-describe('testing create function', ()=>{
+const dummySet = () => {
+    let myset = conset.create((item:DummyItem):string => {
+        return item['test']
+    }, [
+        {test: "a", data: [1]},
+        {test: "b", data: [1,2]},
+        {test: "a", data: [1,2,3]},
+        {test: "c", data: [1,2,3, 4]}
+    ])
+    return myset
+}
+
+describe('create function', ()=>{
     it("should create an empty conset if only comparator provided", ()=>{
 
         let myset = conset.create((item:DummyItem):string => {
@@ -22,4 +41,87 @@ describe('testing create function', ()=>{
         expect(myset.map).to.be.empty
          
     })
+
+    it("should accept an intial set of values while discarding duplicates", () => {
+        let myset = dummySet()
+        expect(isInstanceOfConset(myset)).to.be.equal(true)
+        expect(Object.keys(myset.map).length).to.be.equal(3)
+        expect(myset.order.length).to.be.equal(3)
+    })
+})
+
+describe("remove function", () => {
+    it("should remove the item from map and order", () => {
+        let myset = dummySet()
+        expect(myset.map["a"]).to.be.not.undefined
+        conset.remove({test: "a"}, myset)
+        expect(myset.map["a"]).to.be.undefined
+    })
+    it("should fail silently if item not present", () => {
+        let myset = dummySet()
+        let originalLength = myset.order.length
+        
+        expect(conset.remove({test: 'key-not-present'}, myset))
+            .to.not.throw
+        expect(myset.map["key-not-present"]).to.be.undefined
+        expect(myset.order.length).to.be.equal(originalLength)
+        
+    })
+})
+
+describe("add function", () => {
+    it("should add a new value to the set", () => {
+        let myset = conset.create((item:DummyItem):string => {
+            return item['test']
+        })
+        expect(myset.order.length).to.be.equal(0)
+        conset.add(dummy("key1", [1,2,3,4]), myset)
+        expect(Object.keys(myset.map).length).to.be.equal(1)
+        expect(myset.order.length).to.be.equal(1)
+        
+    })
+
+    it("should override a previously existing key and put it at last position", () => {
+        let myset = conset.create((item:DummyItem):string => {
+            return item['test']
+        })
+        
+        conset.add(dummy('x', [1,2,3]), myset)
+        conset.add(dummy('y', [5,6,7]), myset)
+        conset.add(dummy('z', [5,6,7]), myset)
+        /*check initial order*/
+        expect(myset.order.indexOf('x')).to.be.equal(0)
+        expect(myset.order.indexOf('y')).to.be.equal(1)
+        expect(myset.order.indexOf('z')).to.be.equal(2)
+
+        /* ensure value is overriden */
+        expect(myset.map['x'].data.length).to.be.equal(3)
+        conset.add(dummy('x', [1]), myset)
+        expect(myset.map['x'].data.length).to.be.equal(1)
+
+        /* ensure order is changed */
+        expect(myset.order.indexOf('y')).to.be.equal(0)
+        expect(myset.order.indexOf('z')).to.be.equal(1)
+        expect(myset.order.indexOf('x')).to.be.equal(2)
+        
+    })
+
+    describe("constains function", () => {
+        it("should return true if value exists, otherwise false", () => {
+            let myset = conset.create((item:DummyItem):string => {
+                return item['test']
+            }, [
+                {test: "a", data: [1]},
+                {test: "b", data: [1,2]},
+                {test: "a", data: [1,2,3]},
+                {test: "c", data: [1,2,3, 4]}
+            ])
+
+            expect(conset.contains({test: "a", data: [1]}, myset)).to.be.equal(true)
+            expect(conset.contains({test: "x", data: [1]}, myset)).to.be.equal(false)
+            
+        })
+    })
+    
+
 })
